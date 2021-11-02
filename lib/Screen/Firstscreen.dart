79ea77ad069304e22452e6006b04224279509cf8
Profullow kumar <profullowonline121@gmail.com>
2021-11-02 21:04:16 +0600

    scroling one

--- lib/Screen/Firstscreen.dart
+++ lib/Screen/Firstscreen.dart
@@ -7,100 +7,102 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Image.asset("assets/bergur.jpg"),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 1.0,
-                    vertical: 20,
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.arrow_back,
-                      size: 50,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
                 children: [
-                  Container(
-                    width: 250.0,
-                    child: Text(
-                      "Tasty - Food videos and recipes",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 25,
-                        color: Colors.red,
+                  Image.asset("assets/bergur.jpg"),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 1.0,
+                      vertical: 20,
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back,
+                        size: 50,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                  Icon(
-                    Icons.favorite_border,
-                    color: Colors.red,
-                  ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("⭐ ⭐ ⭐ ⭐ "),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    "It’s can automatically perform various procedures such as meat filling, forming and output and form a fully automatic prepared food line when connects with the battering machine,",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 250.0,
+                      child: Text(
+                        "Tasty - Food videos and recipes",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 25,
+                          color: Colors.red,
+                        ),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    "It’s can automatically perform various procedures such as meat filling, forming and output and form a fully automatic prepared food line when connects with the battering machine,",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
+                    Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                RaisedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.launch, color: Colors.white),
-                  label: Text(
-                    "Order Now",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  color: Colors.blue,
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("⭐ ⭐ ⭐ ⭐ "),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "It’s can automatically perform various procedures such as meat filling, forming and output and form a fully automatic prepared food line when connects with the battering machine,",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "It’s can automatically perform various procedures such as meat filling, forming and output and form a fully automatic prepared food line when connects with the battering machine,",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  RaisedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.launch, color: Colors.white),
+                    label: Text(
+                      "Order Now",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
